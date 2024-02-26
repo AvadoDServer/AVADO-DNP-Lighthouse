31952365720ea2e4c621fd9ae8a04689ad48b8d8
Hendrik Eeckhaut <hendrik@eeckhaut.org>
2024-02-26 22:21:11 +0100

    Holesky lighthouse v5.0.0

--- build/server/server.ts
+++ build/server/server.ts
@@ -3,8 +3,35 @@ import { Status } from "https://deno.land/x/oak_commons@0.5.0/status.ts";
 import { oakCors } from "https://deno.land/x/cors/mod.ts";
 
 import { server_config } from "./server_config.ts";
-import { defaultsettings, read_settings, write_settings } from "./settings.ts";
 import { SupervisorCtl } from "./supervisorctl.ts";
+
+import defaultsettings_gnosis from "./settings/defaultsettings-gnosis.json" with { type: "json" };
+import defaultsettings_holesky from "./settings/defaultsettings-holesky.json" with { type: "json" };
+import defaultsettings_mainnet from "./settings/defaultsettings-mainnet.json" with { type: "json" };
+import defaultsettings_prater from "./settings/defaultsettings-prater.json" with { type: "json" };
+
+const settings_file_path = '/data/settings.json';
+
+export const defaultsettings = () => {
+    switch (server_config.network) {
+        case "gnosis": return defaultsettings_gnosis;
+        case "holesky": return defaultsettings_holesky;
+        case "prater": return defaultsettings_prater;
+        default: return defaultsettings_mainnet;
+    }
+};
+
+export const read_settings = async () => {
+    const text = await Deno.readTextFile(settings_file_path);
+    return JSON.parse(text);
+}
+
+export const write_settings = async (settings: any) => {
+    console.log(settings);
+    console.dir(settings);
+    const text = JSON.stringify(settings, null, 4);
+    await Deno.writeTextFile(settings_file_path, text);
+}
 
 const router = new Router();
 
